18c192a14e31fa0742481a175197b0d2a949623d
Sujan-003 <sujan.vm.dev@gmail.com>
2025-11-14 22:25:17 +0530

    Some work on Main page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,16 @@
+import Image from "next/image"
+import Link from "next/link"
 import { ReactNode } from "react"
 
 const RootLayout = ({children}: {children : ReactNode}) => {
   return (
-    <div>{children}
-    <div>Hey</div>
+    <div className="root-layout">
+        <nav>
+            <Link href='/'>
+            <Image src='/logo.png' alt='logo' width={178} height={152}/>
+            </Link>
+        </nav>
+        {children}
     </div>
   )
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,23 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
-const page = () => {
+const Page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+          <p className="text-lg">
+            Practice real interview questions & get instant feedback
+          </p>
 
-export default page
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Page;

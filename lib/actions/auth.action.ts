bc3998c743ba270feb3c65061595e8fecff379dc
Sujan-003 <sujan.vm.dev@gmail.com>
2025-11-17 19:19:03 +0530

    Sign in and cookie creation

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,4 +1,5 @@
-import { db } from "@/firebase/admin";
+import { auth, db } from "@/firebase/admin";
+import { cookies } from "next/headers";
 
 export async function signUp(params: SignUpParams){
     const{uid, name, email} = params;
@@ -30,3 +31,47 @@ export async function signUp(params: SignUpParams){
     }
 
 }
+
+export async function signIn(params: SignInParams) {
+  const { email, idToken } = params;
+
+  try {
+    const userRecord = await auth.getUserByEmail(email);
+    if (!userRecord)
+      return {
+        success: false,
+        message: "User does not exist. Create an account.",
+      };
+
+    await setSessionCookie(idToken);
+  } catch (error) {
+    console.log(error);
+
+    return {
+      success: false,
+      message: "Failed to log into account. Please try again.",
+    };
+  }
+}
+
+const SESSION_DURATION = 60*60*24*7;
+
+export async function setSessionCookie(idToken: string) {
+  const cookieStore = await cookies();
+
+  // Create session cookie
+  const sessionCookie = await auth.createSessionCookie(idToken, {
+    expiresIn: SESSION_DURATION * 1000, // milliseconds
+  });
+
+  // Set cookie in the browser
+  cookieStore.set("session", sessionCookie, {
+    maxAge: SESSION_DURATION,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+    sameSite: "lax",
+  });
+}
+
+
